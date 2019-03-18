fdff0105d153c14e28a36fa52f49c1ddd57061df
Rob O'Gara <rgo594@gmail.com>
2019-03-18 14:12:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,7 @@
-
 def square_array(array)
-  array2 = 0
-  array.each do |element|
-   puts element ** 2
-   array2 += 1
-  end
+    new_array = []
+    array.each do |element|
+        new_array << element ** 2
+    end
+    new_array
 end
